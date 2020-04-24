394327d22559a2c13bb9ef80b099c109934d6dfe
Sam Carson <37749448+samcarsonx@users.noreply.github.com>
2020-04-24 12:21:06 +0100

    Minified files

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
-    $checkboxes = $("#checkbox-container :checkbox");
-
-$checkboxes.on("change", function(){
-  $checkboxes.each(function(){
-    checkboxValues[this.id] = this.checked;
-  });
-
-  window.localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
-});
-
-// On page load
-$.each(checkboxValues, function(key, value) {
-  $("#" + key).prop('checked', value);
-});
+var checkboxValues=JSON.parse(localStorage.getItem("checkboxValues"))||{},$checkboxes=$("#checkbox-container :checkbox");$checkboxes.on("change",function(){$checkboxes.each(function(){checkboxValues[this.id]=this.checked}),window.localStorage.setItem("checkboxValues",JSON.stringify(checkboxValues))}),$.each(checkboxValues,function(c,e){$("#"+c).prop("checked",e)});
