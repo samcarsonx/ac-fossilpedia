684e1cd55144e49c6f9f4972e6b576f0112e3770
Sam Carson <37749448+samcarsonx@users.noreply.github.com>
2020-04-26 18:33:26 +0100

    Fixed javascript, now it works lol

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues'));
+var checkboxValues = JSON.parse(localStorage.getItem("checkboxValues"));
 if (checkboxValues === null){
   checkboxValues = {};
 }
 console.log(checkboxValues);
-$.each(checkboxValues, function(key, value) {
-  $("#" + key).prop('checked', value);
+
+$(window).on("load", function() {
+  $.each(checkboxValues, function(key, value) {
+    $("#" + key).prop("checked", value);
+  });
 });
 
 $(document).on("change", "input[type='checkbox']", function () {
   $.each($("input[type='checkbox']"), function(){
-    checkboxValues[this.id] = $(this).is(':checked');
+    checkboxValues[this.id] = $("#" + this.id).is(':checked');
   });
-  console.log(checkboxValues);
 
   localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+
+  console.log(checkboxValues);
 });
+
+var reset = function() {
+  location.reload();
+  localStorage.clear();
+};
