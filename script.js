cc31b8430c365e151ebf8623e37a160b1ac5aa09
Sam Carson <37749448+samcarsonx@users.noreply.github.com>
2020-04-30 23:19:20 +0100

    Added language switching
    Deutsch partially complete, will be pushed to gh-pages when ready

--- script.js
+++ script.js
@@ -4,12 +4,6 @@ if (checkboxValues === null){
 }
 console.log(checkboxValues);
 
-$(window).on("load", function() {
-  $.each(checkboxValues, function(key, value) {
-    $("#" + key).prop("checked", value);
-  });
-});
-
 $(document).on("change", "input[type='checkbox']", function () {
   $.each($("input[type='checkbox']"), function(){
     checkboxValues[this.id] = $("#" + this.id).is(':checked');
@@ -20,7 +14,51 @@ $(document).on("change", "input[type='checkbox']", function () {
   console.log(checkboxValues);
 });
 
+var selectedLang = localStorage.getItem("selectedLang");
+if (selectedLang === null){
+  selectedLang = "en";
+}
+$(document).on("change", "select[id='lang']", function () {
+  var selectedLang = $(this).children("option:selected").val();
+  changeText(selectedLang);
+});
+
+var changeText = function(lang) {
+  selectedLang = lang;
+  $("select[id='lang']").val(selectedLang);
+
+  if(selectedLang == "de") {
+    $(".de").show();
+    $(".en").hide();
+  };
+  if(selectedLang == "en") {
+    $(".de").hide();
+    $(".en").show();
+  };
+
+  console.log(selectedLang);
+
+  localStorage.setItem("selectedLang", selectedLang);
+}
+
+$(window).on("load", function() {
+  $.each(checkboxValues, function(key, value) {
+    $("#" + key).prop("checked", value);
+  });
+  changeText(selectedLang);
+});
+
 var reset = function() {
   location.reload();
   localStorage.clear();
+};
+
+var resetText = "";
+var resetDialogue = function() {
+  if (selectedLang == "en") { resetText = "Are you sure? This action cannot be undone." }
+  if (selectedLang == "de") { resetText = "Bist du sicher? Diese Aktion kann nicht rückgängig gemacht werden." }
+
+  if(confirm(resetText)) {
+    reset();
+  };
 };
