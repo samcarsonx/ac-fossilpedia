ac9f8f2d6eb0215bcbdfc9c2e087a735140b9af6
Sam Carson <37749448+samcarsonx@users.noreply.github.com>
2020-05-01 10:47:27 +0100

    Added necessary semicolons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var checkboxValues = JSON.parse(localStorage.getItem("checkboxValues"));
 if (checkboxValues === null){
   checkboxValues = {};
-}
+};
 console.log(checkboxValues);
 
 $(document).on("change", "input[type='checkbox']", function () {
@@ -17,7 +17,7 @@ $(document).on("change", "input[type='checkbox']", function () {
 var selectedLang = localStorage.getItem("selectedLang");
 if (selectedLang === null){
   selectedLang = "en";
-}
+};
 $(document).on("change", "select[id='lang']", function () {
   var selectedLang = $(this).children("option:selected").val();
   changeText(selectedLang);
@@ -39,7 +39,7 @@ var changeText = function(lang) {
   console.log(selectedLang);
 
   localStorage.setItem("selectedLang", selectedLang);
-}
+};
 
 $(window).on("load", function() {
   $.each(checkboxValues, function(key, value) {
@@ -55,8 +55,8 @@ var reset = function() {
 
 var resetText = "";
 var resetDialogue = function() {
-  if (selectedLang == "en") { resetText = "Are you sure? This action cannot be undone." }
-  if (selectedLang == "de") { resetText = "Bist du sicher? Diese Aktion kann nicht rückgängig gemacht werden." }
+  if (selectedLang == "en") { resetText = "Are you sure? This action cannot be undone." };
+  if (selectedLang == "de") { resetText = "Bist du sicher? Diese Aktion kann nicht rückgängig gemacht werden." };
 
   if(confirm(resetText)) {
     reset();
